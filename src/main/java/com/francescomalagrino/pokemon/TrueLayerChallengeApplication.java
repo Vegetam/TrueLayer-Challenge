dced5e3aba6b1e95f56432cfb370517caefeb2f8
Francesco Malagrino <francesco.malagrino@gmail.com>
2020-09-01 18:05:29 +0200

    Create the Spring boot Project
    Create the Spring boot Project

---
+++ src/main/java/com/francescomalagrino/pokemon/TrueLayerChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.francescomalagrino.pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrueLayerChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrueLayerChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/francescomalagrino/pokemon/TrueLayerChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francescomalagrino.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrueLayerChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
