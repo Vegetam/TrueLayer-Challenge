4c12c48624dd62bc01d1e38b69bcadce7b61a01b
Francesco Malagrino <francesco.malagrino@gmail.com>
2020-09-01 18:55:26 +0200

    Creation of the shakespeare model
    Creation of the shakespeare model

---
+++ src/main/java/com/francescomalagrino/clients/shakespeare/model/Contents.java
@@ -0,0 +1,17 @@
+package com.francescomalagrino.clients.shakespeare.model;
+
+public class Contents {
+
+	  private String translated;
+
+	  public Contents() {
+	  }
+
+	  public Contents(String translated) {
+	    this.translated = translated;
+	  }
+
+	  public String getTranslated() {
+	    return translated;
+	  }
+	}
---
+++ src/main/java/com/francescomalagrino/clients/shakespeare/model/Translation.java
@@ -0,0 +1,19 @@
+package com.francescomalagrino.clients.shakespeare.model;
+
+import com.francescomalagrino.clients.shakespeare.model.Contents;
+
+public class Translation {
+
+	  private Contents contents;
+
+	  public Translation() {
+	  }
+
+	  public Translation(Contents contents) {
+	    this.contents = contents;
+	  }
+
+	  public Contents getContents() {
+	    return contents;
+	  }
+	}
